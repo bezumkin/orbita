de8cc2b4314603aaff4a3142931551449bf6230c
bezumkin <bezumkin@yandex.ru>
2024-07-07 12:10:45 +0700

    Fix sitemap directive format in robots.txt

---
+++ frontend/src/server/routes/robots.txt.ts
@@ -0,0 +1,15 @@
+export default defineEventHandler((event) => {
+  setHeader(event, 'content-type', 'application/text')
+  setHeader(event, 'cache-control', 'max-age=3600')
+  const {SITE_URL} = useRuntimeConfig().public
+
+  return String(`
+User-agent: *
+
+Disallow: /admin
+Disallow: /user
+Disallow: /?tags=
+
+Sitemap: ${SITE_URL}sitemap.xml
+`).trim()
+})
