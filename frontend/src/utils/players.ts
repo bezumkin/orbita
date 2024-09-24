3ae4c1115c5ab112548317435592468b378580fc
bezumkin <bezumkin@yandex.ru>
2024-09-24 11:33:13 +0700

    preferNativeHLS only for Safari browsers

--- frontend/src/utils/players.ts
+++ frontend/src/utils/players.ts
@@ -28,7 +28,7 @@ const commonSettings: Partial<MediaPlayerProps> = {
   streamType: 'on-demand',
   logLevel: import.meta.dev ? 'debug' : 'warn',
   playsInline: true,
-  preferNativeHLS: true,
+  preferNativeHLS: import.meta.client && /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
 }
 const layoutSettings: Partial<DefaultLayoutProps> = {
   playbackRates: {min: 0.25, max: 3, step: 0.25},
